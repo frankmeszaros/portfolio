86fb1fb62fff0fcc7df1370b7574d7ad70a61376
frankmeszaros <fmeszaro@gmail.com>
2020-08-22 09:59:18 -0400

    wrapping in decoding

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -7,7 +7,7 @@ const httpLink = createHttpLink({ uri: GITHUB_URI });
 
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
-  const token = process.env.REACT_APP_GITHUB_TOKEN;
+  const token = atob(process.env.REACT_APP_GITHUB_TOKEN);
 
   // return the headers to the context so httpLink can read them
   return {
